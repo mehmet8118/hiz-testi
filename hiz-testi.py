5018683407e3da092386284681380ad5ba9cb824
D3R3LI <mehmet81_gsss@hotmail.com>
2019-12-15 21:11:05 +0300

    Update hiz-testi.py

--- hiz-testi.py
+++ hiz-testi.py
@@ -7,3 +7,6 @@ baslangic = int(time.time())
 istek = requests.get("http://ipv4.download.thinkbroadband.com/5MB.zip",allow_redirects=True)
 end = int(time.time())
 print("%d saniye sürede 5MB dosya indirildi. Hızınız: %s" % (end - baslangic,(str(int((5*1024) / (end - baslangic))) + "kb/s")))
+
+
+#liser-tht Alıntı
